0e274dc8a37419b0570661501c64781a85d05eb5
maker99 <maker99@users.noreply.github.com>
2019-06-06 08:02:29 +0200

    fixing include path

--- zeal60/keycode.cpp
+++ zeal60/keycode.cpp
@@ -7,11 +7,11 @@
 
 #include "keycode.h"
 
-#include "../../qmk_firmware/tmk_core/common/keycode.h"
-#include "../../qmk_firmware/quantum/quantum_keycodes.h"
-#include "../../qmk_firmware/tmk_core/common/action_code.h"
-#include "../../qmk_firmware/keyboards/zeal60/rgb_backlight_keycodes.h"
-#include "../../qmk_firmware/keyboards/zeal60/zeal60_keycodes.h"
+#include "../qmk_firmware/tmk_core/common/keycode.h"
+#include "../qmk_firmware/quantum/quantum_keycodes.h"
+#include "../qmk_firmware/tmk_core/common/action_code.h"
+#include "../qmk_firmware/keyboards/zeal60/rgb_backlight_keycodes.h"
+#include "../qmk_firmware/keyboards/zeal60/zeal60_keycodes.h"
 
 #ifdef ZEALM104
 #include "keyboards/M/M104/rgb_backlight_api.h"
